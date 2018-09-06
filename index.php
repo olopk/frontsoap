2bc694ac347f36990ba4e59eab7de6a55c784af4
olopk <olekwojas@gmail.com>
2018-09-06 14:54:41 +0200

    first

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>MF Soap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Gamja+Flower" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+</head>
+<body>
+    <div class="container-fluidX">
+        <div id="logo">
+            <h1>Soap MF</h1>
+            <h2>Check contractor status app</h2>
+        </div>
+        <div id="login">
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Login</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Podaj e-mail">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Hasło</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Hasło">
+                </div>
+                <button type="submit" class="btn btn-primary">Zaloguj</button>
+            </form>
+        </div> 
+    </div
+<script src="script.js"></script>    
+</body>
+</html>
